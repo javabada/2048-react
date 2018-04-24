2920f934d3869689e2d9351c7492d57b02e0d68a
Jeffrey Zhang <jzhang11235@gmail.com>
2018-04-24 19:42:26 +1000

    Create grid

--- src/App.js
+++ src/App.js
@@ -1,18 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function Cell(props) {
+  return (
+    <div className="cell">
+      {props.value}
+    </div>
+  );
+}
+
+class Grid extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grid: Array(4).fill(Array(4).fill(0)),
+    };
+  }
+
+  renderCell(x, y) {
+    return (
+      <Cell value={this.state.grid[x][y]} />
+    );
+  }
+
+  render() {
+    return (
+      <div className="grid">
+        <div className="grid-row">
+          {this.renderCell(0, 0)}
+          {this.renderCell(0, 1)}
+          {this.renderCell(0, 2)}
+          {this.renderCell(0, 3)}
+        </div>
+        <div className="grid-row">
+          {this.renderCell(1, 0)}
+          {this.renderCell(1, 1)}
+          {this.renderCell(1, 2)}
+          {this.renderCell(1, 3)}
+        </div>
+        <div className="grid-row">
+          {this.renderCell(2, 0)}
+          {this.renderCell(2, 1)}
+          {this.renderCell(2, 2)}
+          {this.renderCell(2, 3)}
+        </div>
+        <div className="grid-row">
+          {this.renderCell(3, 0)}
+          {this.renderCell(3, 1)}
+          {this.renderCell(3, 2)}
+          {this.renderCell(3, 3)}
+        </div>
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <Grid />
       </div>
     );
   }
