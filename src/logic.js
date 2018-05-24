14e09af29b54d1bef958aaabb83d3c0970c7651b
Jeffrey Zhang <jzhang11235@gmail.com>
2018-05-24 20:31:57 +1000

    Implement add tile

--- src/logic.js
+++ src/logic.js
@@ -1,4 +1,20 @@
-export default function left(tiles) {
+export function add(tiles) {
+  const emptyCells = [];
+  for (let x = 0; x < 4; x += 1) {
+    for (let y = 0; y < 4; y += 1) {
+      if (!tiles.some(tile => tile.x === x && tile.y === y)) {
+        emptyCells.push({ x, y });
+      }
+    }
+  }
+  const cell = emptyCells[Math.floor(Math.random() * emptyCells.length)];
+  const value = Math.random() > 0.9 ? 4 : 2;
+  const after = tiles.slice();
+  after.push({ ...cell, value });
+  return after;
+}
+
+export function left(tiles) {
   // sort array so that tiles are read in the right order
   tiles.sort((a, b) => (a.y === b.y ? a.x - b.x : a.y - b.y));
 
--- src/logic.test.js
+++ src/logic.test.js
@@ -1,4 +1,34 @@
-import left from './logic';
+import { add, left } from './logic';
+
+describe('add tile', () => {
+  it('adds a new tile to the grid', () => {
+    const tiles = [];
+    const result = add(tiles);
+    expect(result.length).toBe(1);
+  });
+
+  it('adds the new tile to an empty cell', () => {
+    const tiles = [
+      { x: 0, y: 0, value: 2 },
+      { x: 1, y: 0, value: 2 },
+      { x: 2, y: 0, value: 2 },
+      { x: 3, y: 0, value: 2 },
+      { x: 0, y: 1, value: 2 },
+      { x: 2, y: 1, value: 2 },
+      { x: 3, y: 1, value: 2 },
+      { x: 0, y: 2, value: 2 },
+      { x: 1, y: 2, value: 2 },
+      { x: 2, y: 2, value: 2 },
+      { x: 3, y: 2, value: 2 },
+      { x: 0, y: 3, value: 2 },
+      { x: 1, y: 3, value: 2 },
+      { x: 2, y: 3, value: 2 },
+      { x: 3, y: 3, value: 2 },
+    ];
+    const result = add(tiles);
+    expect(result.some(tile => tile.x === 1 && tile.y === 1)).toBe(true);
+  });
+});
 
 describe('move left', () => {
   it('moves a tile to the edge', () => {
