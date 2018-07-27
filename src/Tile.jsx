0181cdd002da0d25d870d92f3e08578726eff622
Jeffrey Zhang <jzhang11235@gmail.com>
2018-07-28 00:33:09 +1000

    Add animations

--- src/Tile.jsx
+++ src/Tile.jsx
@@ -16,13 +16,55 @@ const propTypes = {
 const defaultProps = {
   previousX: null,
   previousY: null,
-  onMerge: null,
+  onMerge: () => {},
 };
 
 const Tile = class extends React.Component {
   constructor(props) {
     super(props);
     this.ref = React.createRef();
+  }
+
+  componentDidMount() {
+    const {
+      x,
+      y,
+      previousX,
+      previousY,
+      animation,
+      onMerge,
+    } = this.props;
+
+    let keyframes = [];
+    switch (animation) {
+      case 'add':
+        keyframes = [
+          { transform: 'scale(0)' },
+          { transform: 'scale(1)' },
+        ];
+        break;
+      case 'move':
+      case 'mergeStart':
+        keyframes = [
+          { ...getTilePosition(previousX, previousY) },
+          { ...getTilePosition(x, y) },
+        ];
+        break;
+      case 'mergeEnd':
+        keyframes = [
+          { transform: 'scale(1)' },
+          { transform: 'scale(1.2)' },
+          { transform: 'scale(1)' },
+        ];
+        break;
+      default:
+    }
+
+    // animate() breaks tests, comment out the following block before running
+    const animationObj = this.ref.current.animate(keyframes, 100);
+    if (animation === 'mergeStart') {
+      animationObj.onfinish = onMerge;
+    }
   }
 
   render() {
